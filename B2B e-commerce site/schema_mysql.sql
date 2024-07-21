fbecf3acc8856370c27c3a888983fe130bbd6e06
surya <surya1.singh@gmail.com>
2024-07-21 19:23:44 -0400

    physical schema for mysql

---
+++ B2B e-commerce site/schema_mysql.sql
@@ -0,0 +1,184 @@
+drop database b2b1;
+create database b2b1;
+use b2b1;
+CREATE TABLE companies (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  cuit int NOT NULL,
+  name varchar(255) NOT NULL,
+  updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY cuit (cuit)
+);
+
+
+CREATE TABLE company_addresses (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    company_id INT NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    zip_code VARCHAR(20) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    address_type VARCHAR(20) NOT NULL, -- e.g., 'billing', 'shipping'
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (company_id) REFERENCES companies(id),
+    UNIQUE (company_id, address_type)
+);
+
+
+CREATE TABLE suppliers (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    company_id INT UNIQUE NOT NULL,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (company_id) REFERENCES companies(id)
+);
+
+
+CREATE TABLE product_categories (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP
+);
+-- Assumption: one product belongs to one category. for many to many relationship another table will be required
+
+
+CREATE TABLE products (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    supplier_id INT NOT NULL,
+    product_category_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    default_price DECIMAL(10, 2) NOT NULL,
+    is_active boolean default true,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (supplier_id) REFERENCES suppliers(id),
+    FOREIGN KEY (product_category_id) REFERENCES product_categories(id)
+);
+
+
+CREATE TABLE catalogs (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    company_id INT NOT NULL,
+    is_active boolean default true,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (company_id) REFERENCES companies(id)
+);
+
+
+CREATE TABLE catalog_items (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    catalog_id INT NOT NULL,
+    product_id INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (catalog_id) REFERENCES catalogs(id),
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    UNIQUE (catalog_id, product_id)
+);
+
+
+CREATE TABLE discounts (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    catalog_item_id INT,
+    discount_percentage DECIMAL(5, 2) CHECK (discount_percentage >= 0 AND discount_percentage <= 100),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (catalog_item_id) REFERENCES catalog_items(id)
+);
+
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE user_addresses (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    zip_code VARCHAR(20) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    address_type VARCHAR(20) NOT NULL, -- e.g., 'billing', 'shipping'
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    UNIQUE (user_id, address_type)
+);
+
+CREATE TABLE orders (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+CREATE TABLE order_items (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    catalog_item_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES Orders(id),
+    FOREIGN KEY (catalog_item_id) REFERENCES catalog_items(id)
+);
+
+
+
+CREATE TABLE reviews (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_item_id INT NOT NULL,
+    user_id INT NOT NULL,
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    review TEXT,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_item_id) REFERENCES order_items(id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+
+
+CREATE TABLE payments (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_method VARCHAR(50) NOT NULL, -- e.g., 'credit_card', 'bank_transfer'
+    status VARCHAR(50) NOT NULL, -- e.g., 'pending', 'completed', 'failed'
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES Orders(id)
+);
+
+
+CREATE TABLE shipment_tracking (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    tracking_number VARCHAR(100) UNIQUE NOT NULL,
+    carrier VARCHAR(100) NOT NULL,
+    status VARCHAR(50) NOT NULL, -- e.g., 'in_transit', 'delivered'
+    estimated_delivery DATE,
+    updated_at timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES Orders(id)
+);
